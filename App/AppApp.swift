38668cf97f78450b4267288c916de1de99afd5dc
ERA <ray810815@gmail.com>
2025-05-11 21:19:32 -0400

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Raymond Lei on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
